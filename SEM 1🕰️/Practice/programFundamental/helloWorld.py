79e8f957799f1f90c0f7b659f3d5ec6ab9da3d61
aswin-kumar-1999 <kumarsaswin10@gmail.com>
2024-03-12 13:15:45 +1100

    program fundaments | PA

---
+++ SEM 1🕰️/Practice/programFundamental/helloWorld.py
@@ -0,0 +1,24 @@
+print("Hello world")
+
+val=10
+print(type(val))
+val="10"
+print(type(val))
+
+score=10
+print(score)
+score+=5
+print(score)
+
+import sys
+sys.stdout.write("Let us do some calculations.\n")
+var_a = 5
+var_b = 10
+var_c = var_a + var_b
+sys.stdout.write(str(var_c))
+
+sys.stdout.write("\nEnter your name \n")
+userName=str(sys.stdin.readline().strip())
+userName1=input("Enter the last name \n")
+sys.stdout.write("Hello " + userName + userName1)
+
